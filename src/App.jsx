49888e61ce4fd3eccbec9b42079d0f4c5c9326d1
niiquash <qua16006@byui.edu>
2025-01-16 18:39:29 -0700

    added react-confetti.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,56 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import ReactConfetti from "react-confetti";
 
 const App = () => {
+  const confetti = () => {
+    return <ReactConfetti />;
+  };
   const generateAllNewDice = () => {
-    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+    return new Array(10).fill(0).map(() => ({
+      id: nanoid(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    }));
   };
 
   const [dice, setDice] = useState(generateAllNewDice());
 
-  const diceElements = dice.map((num, idx) => <Die key={idx} value={num} />);
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
 
-  const getNewDice = () => {
-    setDice(generateAllNewDice());
+  const rollDice = () => {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        !die.isHeld ? { ...die, value: Math.ceil(Math.random() * 6) } : die
+      )
+    );
   };
+
+  const hold = (id) => {
+    setDice((prevDice) =>
+      prevDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
+  };
+
+  const diceElements = dice.map((num) => (
+    <Die
+      handleClick={() => {
+        hold(num.id);
+      }}
+      key={num.id}
+      value={num.value}
+      isHeld={num.isHeld}
+    />
+  ));
+
   return (
     <main className="main">
+      {gameWon && confetti()}
       <section className="main__instructions">
         <h2>Tenzies</h2>
         <p>
@@ -25,8 +61,8 @@ const App = () => {
 
       <div className="main__dice--container">{diceElements}</div>
 
-      <button onClick={getNewDice} className="main__roll--button">
-        Roll
+      <button onClick={rollDice} className="main__roll--button">
+        {gameWon ? "New Game" : "Roll"}
       </button>
     </main>
   );
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Die = ({ value }) => {
-  return <button className="main__dice">{value}</button>;
+const Die = ({ value, isHeld, handleClick }) => {
+  const styles = { backgroundColor: isHeld ? "#59e391" : "#ffffff" };
+
+  return (
+    <button style={styles} onClick={handleClick} className="main__dice">
+      {value}
+    </button>
+  );
 };
 
 export default Die;
